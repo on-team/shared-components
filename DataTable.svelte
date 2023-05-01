152ad5cefea7a842d7f08e7e1f8b66f49bb10787
seiichi-shinomiya <seiichi.shinomiya@o-n.co.jp>
2023-05-01 11:17:39 +0900

    fix: DataTableの行内のボタン押下時にrowHrefの遷移も起こる不具合を修正

--- DataTable.svelte
+++ DataTable.svelte
@@ -244,7 +244,12 @@
         class="body-row"
         href={rowHref?.(row)}
         on:click={(event) => {
-          if (isNestedClickEvent(event)) return
+          if (isNestedClickEvent(event)) {
+            // 例えば行内のボタン押下時に画面遷移を暴発させないために必要
+            event.preventDefault()
+            return
+          }
+
           return onClickRow?.(row)
         }}
       >
